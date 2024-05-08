11c3895c874319883a1a1a567c9d8c0271374ece
superb-shan <shankar.k2021csec@sece.ac.in>
2024-05-08 16:39:23 +0530

    backend api added

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import LoginSignup from "./components/pages/LoginSignup";
 import { useDispatch, useSelector } from "react-redux";
 import { setAuth } from "./slices/userSlice";
 
-axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.baseURL = 'https://adya-e-commerce.onrender.com/api';
 
 
 function App() {
